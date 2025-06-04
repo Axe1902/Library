d5c5645e31010c7cc97aae8f33639fbdf39d76d7
Nikita <nikita-bar@bk.ru>
2025-06-04 12:20:20 +0300

    Добавление логгирования и возможности тестировать api через swagger. Доработка контроллера для книг

--- src/main/java/com/example/library/controllers/AuthorController.java
+++ src/main/java/com/example/library/controllers/AuthorController.java
@@ -4,6 +4,8 @@ import com.example.library.model.dto.AuthorDto;
 import com.example.library.model.entity.Author;
 import com.example.library.model.service.IAuthorService;
 import com.example.library.model.utils.IDtoMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/authors")
 public class AuthorController
 {
+    private static final Logger logger = LoggerFactory.getLogger(AuthorController.class);
+
     private final IAuthorService authorService;
     private final IDtoMapper dtoMapper;
 
@@ -31,16 +35,28 @@ public class AuthorController
             @RequestParam(value = "limit", defaultValue = "20") int limit
     )
     {
+        logger.info("Получен запрос на получение списка авторов.");
+        logger.debug("Страница: {}, лимит: {}", page, limit);
+
         Pageable pageable = PageRequest.of(page, limit);
 
-        return authorService.findAll(pageable).map(dtoMapper::toDto);
+        var result = authorService.findAll(pageable).map(dtoMapper::toDto);
+
+        logger.info("Список авторов получен.");
+        logger.debug(result.stream().toList().toString());
+
+        return result;
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<AuthorDto> getAuthorById(@PathVariable Integer id)
     {
+        logger.info("Получен запрос на получение автора по идентификатору: {}.", id);
+
         Author result = authorService.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Cannot find author with id=" + id));
+
+        logger.info("Автор с идентификатором {} получен.", id);
 
         return ResponseEntity.ok(dtoMapper.toDto(result));
     }
@@ -49,7 +65,11 @@ public class AuthorController
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Void> addAuthor(@RequestBody AuthorDto author)
     {
+        logger.info("Получен запрос на добавление нового автора");
+
         authorService.save(dtoMapper.toEntity(author));
+
+        logger.info("Автор успешно добавлен.");
 
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
@@ -60,6 +80,8 @@ public class AuthorController
         var updatedAuthor = dtoMapper.toEntity(updatedAuthorDto);
 
         var id = updatedAuthor.getId();
+
+        logger.info("Получен запрос на обновление информации об авторе с идентификатором: {}.", id);
 
         authorService.findById(id)
                 .map(author -> {
@@ -73,12 +95,18 @@ public class AuthorController
                 })
                 .orElseThrow(() -> new ResourceNotFoundException("Cannot find author with id=" + id));
 
+        logger.info("Информация обновлена.");
+
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
     public void deleteAuthor(@PathVariable Integer id)
     {
+        logger.info("Получен запрос на удаление автора по идентификатору: {}.", id);
+
         authorService.deleteById(id);
+
+        logger.info("Автор удален.");
     }
 }
--- src/main/java/com/example/library/controllers/BookController.java
+++ src/main/java/com/example/library/controllers/BookController.java
@@ -1,9 +1,13 @@
 package com.example.library.controllers;
 
 import com.example.library.model.dto.BookDto;
+import com.example.library.model.entity.Author;
 import com.example.library.model.entity.Book;
+import com.example.library.model.service.IAuthorService;
 import com.example.library.model.service.IBookService;
 import com.example.library.model.utils.IDtoMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -18,11 +22,15 @@ import java.util.List;
 @RequestMapping("/api/books")
 public class BookController
 {
+    private static final Logger logger = LoggerFactory.getLogger(BookController.class);
+
+    private final IAuthorService authorService;
     private final IBookService bookService;
     private final IDtoMapper dtoMapper;
 
-    public BookController(IBookService bookService, IDtoMapper dtoMapper)
+    public BookController(IAuthorService authorService, IBookService bookService, IDtoMapper dtoMapper)
     {
+        this.authorService = authorService;
         this.bookService = bookService;
         this.dtoMapper = dtoMapper;
     }
@@ -33,45 +41,80 @@ public class BookController
             @RequestParam(value = "limit", defaultValue = "20") int limit
     )
     {
+        logger.info("Получен запрос на получение списка книг.");
+        logger.debug("Страница: {}, лимит: {}", page, limit);
+
         Pageable pageable = PageRequest.of(page, limit);
 
-        return bookService.findAll(pageable).map(dtoMapper::toDto);
+        var result = bookService.findAll(pageable).map(dtoMapper::toDto);
+
+        logger.info("Список книг получен.");
+        logger.debug(result.stream().toList().toString());
+
+        return result;
     }
 
     @GetMapping("/last10")
     public List<BookDto> getLastAddedBooks()
     {
-        return bookService.findTop10ByOrderByIdDesc().stream().map(dtoMapper::toDto).toList();
+        logger.info("Получен запрос на получение последних 10 добавленных книг.");
+
+        var result = bookService.findTop10ByOrderByIdDesc().stream().map(dtoMapper::toDto).toList();
+
+        logger.info("Список книг получен.");
+        logger.debug(result.toString());
+
+        return result;
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<BookDto> getBookById(@PathVariable Integer id)
     {
+        logger.info("Получен запрос на получение книги с идентификатором {}.", id);
+
         Book result = bookService.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Book not found with id " + id));
+
+        logger.info("Книга с идентификатором {} получена.", id);
 
         return ResponseEntity.ok(dtoMapper.toDto(result));
     }
 
-    @GetMapping("/author/{id}")
+    @GetMapping("/author/{authorId}")
     public List<BookDto> getBooksByAuthorId(@PathVariable Integer authorId)
     {
+        logger.info("Получен запрос на получение книги по идентификатору автора: {}.", authorId);
+
         return bookService.findByAuthorId(authorId).stream().map(dtoMapper::toDto).toList();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<Void> addBook(@PathVariable Book book)
+    public ResponseEntity<Void> addBook(@RequestBody BookDto book)
     {
-        bookService.save(book);
+        logger.info("Получен запрос на добавление новой книги.");
+
+        Author author = authorService.findById(book.authorId())
+                .orElseThrow(() -> new ResourceNotFoundException("Для добавления книги не удалось найти автора с id " + book.authorId()));
+
+        bookService.save(dtoMapper.toEntity(book, author));
+
+        logger.info("Книга добавлена.");
 
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PostMapping("/update/{id}")
-    public ResponseEntity<Void> updateBook(@PathVariable Book updatedBook)
+    public ResponseEntity<Void> updateBook(@RequestBody BookDto updatedBookDto)
     {
+        Author author = authorService.findById(updatedBookDto.authorId())
+                .orElseThrow(() -> new ResourceNotFoundException("При обновлении информации о книге не удалось найти автора с id " + updatedBookDto.authorId()));
+
+        var updatedBook = dtoMapper.toEntity(updatedBookDto, author);
+
         var id = updatedBook.getId();
+
+        logger.info("Получен запрос на обновление информации о книге с идентификатором {}.", id);
 
         bookService.findById(id)
                 .map(book -> {
@@ -82,12 +125,18 @@ public class BookController
                 })
                 .orElseThrow(() -> new ResourceNotFoundException("Book not found with id " + id));
 
+        logger.info("Информация о книге с идентификатором {} обновлена.", id);
+
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
     public void deleteBook(@PathVariable Integer id)
     {
+        logger.info("Получен запрос на удаление книги по идентификатору: {}.", id);
+
         bookService.deleteById(id);
+
+        logger.info("Книга с идентификатором {} удалена.", id);
     }
 }
